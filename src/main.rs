9076f0ea0f0e1d40295ee866d67abaab229663e1
Sumit Patel <sumitpatel@Sumits-MacBook-Pro.local>
2024-11-29 10:31:58 +0530

    Modify path.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let db_file_path = "/Users/sumitpatel/Work/Rust/database/sand/sand.db";
+    let db_file_path = "../sand.db";
     let mut file = File::open(db_file_path)?;
     let mut buffer = [0u8; 100];
     file.read_exact(&mut buffer)?;
@@ -12,7 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_header = page::page_parser::parse_file_header(&buffer)?;
     println!("{:#?}", file_header);
 
-    let page_header = page::page_parser::parse_page_header(&mut file);
+    let page_header = page::page_parser::parse_page_header(&mut file)?;
     println!("{:#?}", page_header);
 
     Ok(())
